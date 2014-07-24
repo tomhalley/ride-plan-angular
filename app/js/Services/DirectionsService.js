6dc57d918e3788966fd126113ba0f2271bfb3500
Tom Halley <tom.halley@nccgroup.com>
2014-07-24 17:29:33 +0100

    STILL Trying to get Direction API working

--- app/js/Services/DirectionsService.js
+++ app/js/Services/DirectionsService.js
@@ -2,18 +2,21 @@
 
 angular.module('MotoNet.Services', [])
     .config(function($httpProvider) {
-        //Enable cross domain calls
         $httpProvider.defaults.useXDomain = true;
-
-        //Remove the header used to identify ajax call  that would prevent CORS from working
-        delete $httpProvider.defaults.headers.common['X-Requested-With'];
+        //delete $httpProvider.defaults.headers.common['X-Requested-With'];
+        delete $httpProvider.defaults.headers.common['X-XSS-Protection'];
     })
     .service('DirectionService', function($http) {
         this.getGoogleApiResponse = function() {
             return $http({
-                dataType: "jsonp",
+                dataType: "json",
                 method: "GET",
-                url: "http://maps.googleapis.com/maps/api/directions/nav?origin=Guildford&destination=Brighton",
+                url: "http://maps.googleapis.com/maps/api/directions/json",
+                params: {
+                    origin: "Guildford",
+                    destination: "Brighton",
+                    key: "AIzaSyBWuYgeB2ELhf8YNTwwRqzDY_r3gTGVBIc"
+                }
             })
         }
     });
