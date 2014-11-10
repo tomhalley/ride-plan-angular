67a623a553eb11d2a70df324113d67d8877e1d83
Tom Halley <tom.halley@nccgroup.com>
2014-11-10 17:51:16 +0000

    gulp build now works

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 var gulp = require('gulp'),
     w3cjs = require('gulp-w3cjs'),
     uglify = require("gulp-uglify"),
-    karma = require('karma').server;
+    karma = require('karma').server,
+    ngmin = require('gulp-ngmin'),
+    concat = require('gulp-concat');
 
 gulp.task('w3c', function() {
     gulp.src('app/*.html')
@@ -10,7 +12,9 @@ gulp.task('w3c', function() {
 
 gulp.task('build', function() {
     gulp.src('app/js/**/*.js')
-        .pipe(uglify());
+        .pipe(uglify())
+        .pipe(concat("dist/build-file.js"))
+        .pipe(gulp.dest(''));
 });
 
 gulp.task('start', function() {
