2153f117d266ea70827d318d73a3bd154d0431b5
Tom Halley <tom.halley@nccgroup.com>
2014-11-11 15:03:52 +0000

    Fixed broken unit test runner

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -4,12 +4,12 @@ module.exports = function(config){
     basePath : '../',
 
     files : [
-        'bower_components/angular/angular.js',
+        'lib/angular/angular.js',
         'http://maps.googleapis.com/maps/api/js?sensor=false&language=en',
-        'bower_components/angular-mocks/angular-mocks.js',
-        'bower_components/angular-route/angular-route.js',
-        'bower_components/lodash/dist/lodash.underscore.js',
-        'bower_components/angular-google-maps/dist/angular-google-maps.min.js',
+        'lib/angular-mocks/angular-mocks.js',
+        'lib/angular-route/angular-route.js',
+        'lib/lodash/dist/lodash.underscore.js',
+        'lib/angular-google-maps/dist/angular-google-maps.min.js',
         'src/app.js',
         'src/**/*.js',
         'test/unit/**/*.js'
