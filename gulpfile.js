7ec73ebba8b53b930dec438d829fdb64d614be42
Tom Halley <tom.halley@nccgroup.com>
2014-11-11 13:13:58 +0000

    Fixed Gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,66 +4,65 @@ var gulp = require('gulp'),
     karma = require('karma').server,
     concat = require('gulp-concat'),
     webserver = require('gulp-webserver'),
-    ngmin = require("gulp-ngmin"),
+    ng_annotate = require('gulp-ng-annotate'),
     minifycss = require('gulp-minify-css'),
-    changed = require('gulp-changed');
+    changed = require('gulp-changed'),
+    bower = require('gulp-bower'),
+    size = require('gulp-size');
 
 var js_vendor_files = [
-    "bower_components/html5-boilerplate/js/vendor/modernizr-2.6.2.min.js",
     "bower_components/jquery/dist/jquery.min.js",
     "bower_components/jquery-ui/jquery-ui.min.js",
     "bower_components/jquery-ui/ui/sortable.js",
     "bower_components/angular/angular.min.js",
     "bower_components/angular-facebook/lib/angular-facebook.js",
     "bower_components/angular-translate/angular-translate.min.js",
-    "bower_components/angular-bootstrap/ui-bootstrap.min.js",
     "bower_components/angular-bootstrap/ui-bootstrap-tpls.min.js",
     'bower_components/lodash/dist/lodash.underscore.min.js',
     'bower_components/angular-google-maps/dist/angular-google-maps.min.js',
-    "bower_components/bootstrap/dist/js/bootstrap.min.js",
     "bower_components/angular-ui-router/release/angular-ui-router.min.js",
     "bower_components/angular-ui/build/angular-ui.min.js",
-    "bower_components/angular-ui/modules/directives/sortable/sortable.js",
     "bower_components/angular-dialogs-service/dialogs.min.js",
     "bower_components/angular-sanitize/angular-sanitize.min.js"
 ];
 
 var css_vendor_files = [
-    "bower_components/html5-boilerplate/css/normalize.css",
-    "bower_components/html5-boilerplate/css/main.css",
     "bower_components/bootstrap/dist/css/bootstrap.min.css",
     "bower_components/angular-dialogs-service/dialogs.min.css",
     "app/css/app.css"
 ];
 
-gulp.task('w3c', function() {
-    gulp.src('app/*.html')
-        .pipe(w3cjs());
+gulp.task('install', function() {
+    return bower()
+        .pipe(gulp.dest('bower_components/'));
 });
 
-gulp.task('build', function() {
-    gulp.src('src/**/*.js')
-        .pipe(concat("ride-plan.js"))
-        .pipe(changed("tmp/"))
-        .pipe(gulp.dest("tmp/"))
-        .pipe(ngmin())
-        .pipe(uglify({mangle: false}))
-        .pipe(gulp.dest('app/js/'));
-
+gulp.task('build-vendor-js', function() {
     gulp.src(js_vendor_files, {base: 'bower_components/'})
         .pipe(concat("vendor.js"))
-        .pipe(changed("tmp/"))
-        .pipe(gulp.dest("tmp/"))
         .pipe(uglify())
+        .pipe(size({title: "Vendor js - compressed"}))
         .pipe(gulp.dest('app/js/'));
+});
 
+gulp.task('build-app-js', function() {
+    gulp.src('src/**/*.js')
+        .pipe(concat("ride-plan.js"))
+        .pipe(ng_annotate())
+        .pipe(uglify({mangle: false}))
+        .pipe(size({title: "App js - compressed"}))
+        .pipe(gulp.dest('app/js/'));
+});
+
+gulp.task('build-css', function() {
     gulp.src(css_vendor_files)
         .pipe(concat("styles.css"))
-        .pipe(changed("tmp/"))
-        .pipe(gulp.dest("tmp/"))
         .pipe(minifycss())
+        .pipe(size({title: "CSS - compressed"}))
         .pipe(gulp.dest('app/css/'));
 });
+
+gulp.task('build', ['build-vendor-js', 'build-app-js', 'build-css']);
 
 gulp.task('start', ['build'], function() {
     gulp.src('app')
@@ -74,7 +73,12 @@ gulp.task('start', ['build'], function() {
         }));
 });
 
-gulp.task('test', function() {
+gulp.task('w3c', function() {
+    gulp.src('app/*.html')
+        .pipe(w3cjs());
+});
+
+gulp.task('test', ['w3c'], function() {
     karma.start({
         configFile: __dirname + '/test/karma.conf.js',
         singleRun: true
